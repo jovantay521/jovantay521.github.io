e13d72d65f86a974bccced1a48df785dbebb9f7d
xTakoyaki <beliviankonglien@gmail.com>
2024-04-13 14:41:15 +0800

    Revert "Merge remote-tracking branch 'origin/bohui_branch' into dev-branch"
    This reverts commit 996da0a67e1d0ccbecd3cdcdbc85b53ea2683e9f, reversing
    changes made to 099f95bb41095e0e103ae4b9b39bec4dc1cf5b80.

--- backends/login.py
+++ backends/login.py
@@ -1,25 +1,9 @@
-from flask import Blueprint, render_template, request, session, redirect
-from database.dbAccOperation import  dbAccOp
+from flask import Blueprint, render_template, request, redirect, url_for
 
 loginBp = Blueprint("loginBp",__name__)
 
-#renders page
 @loginBp.route("/login", methods =['GET','POST'])
 def login():
 
     return render_template('login.html')
 
-#onclick event on form submission
-@loginBp.post("/accLogin")
-def accLogin_post():
-    email = request.form['email']
-    pwd = request.form['password']
-
-    userdetails = [email, pwd]
-    result = dbAccOp.accLogin(userdetails)
-    if (result != 0):
-        # print("Account " + username + "has logged in")
-        session['username'] = email
-        return redirect("/home")
-    else:
-        return redirect("/login")
