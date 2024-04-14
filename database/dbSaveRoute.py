064247955345838ce29d99b8f6cd653bf9d31ea5
rayrex7 <bohui1523@gmail.com>
2024-04-14 22:31:07 +0800

    Completed database backend code implementation to save a route and retrieve user's saved routes

--- database/dbSaveRoute.py
+++ database/dbSaveRoute.py
@@ -14,17 +14,35 @@ class dbSaveRoute:
         savedRoutes = db.child("users").child(uid).child("SavedRoutes").get()
 
         limit = dbSaveRoute.checkSlotLimit(savedRoutes)
-        # if (limit <= 9):
-        #     #Update user's save slot with newly created route
-        #     # db.child("users").child(uid).child("SavedRoutes").child(routename).update(routeData)
-        #     print("success")
-        # else:
-        #     print("failed")
+        if (limit <= 9):
+            #Update user's save slot with newly created route
+            db.child("users").child(uid).child("SavedRoutes").child(routename).update(routeData)
+            print("success")
+        else:
+            print("failed")
+
+    @staticmethod
+    def retrieveSaveRoute(self):
+        #Open database connection
+        firebase = dbConnection.openConn()
+        #Access firebase database
+        db = firebase.database()
+
+        uid = session['uid']
+        savedRoutes = db.child("users").child(uid).child("SavedRoutes").get()
+
+        return savedRoutes
+
 
     @staticmethod
     def checkSlotLimit(savedata):
         count =0
-        # if (savedata.val() is not None):
-        #
-        # else:
-        #     return 0
+        if (savedata.val() is not None):
+            for save in savedata.each():
+                if (save is not None):
+                    count += 1
+
+            return count
+        else:
+            return 0
+
